0921a865f03d02489c0acdbcfaf91764424545c9
emarkula24 <emarkula24@student.oulu.fi>
2025-01-25 12:43:45 +0200

    removed unnecessary files

--- built/src/Calculators/calculate_small_order_surcharge.js
+++
@@ -1,7 +0,0 @@
-export default function calculate_small_order_surcharge(order_minimum_no_surcharge, cart_value) {
-    let small_order_surcharge = order_minimum_no_surcharge - cart_value;
-    if (small_order_surcharge < 0) {
-        small_order_surcharge = 0;
-    }
-    return small_order_surcharge;
-}
--- built/src/routes/priceRouter.js
+++
@@ -1,5 +0,0 @@
-import Router from "express";
-import { getDeliveryOrderPrice } from "../controllers/priceController.js";
-const router = Router();
-router.get("/", getDeliveryOrderPrice);
-export default router;
